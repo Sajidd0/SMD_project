37641985fc26df567bdcb5708dfd1eb112c87dca
Sajid Saleem <sajidsaleem864@gmail.com>
2022-05-23 02:03:48 +0500

    error resolve

--- app/src/main/java/com/example/testdemo/ideapage.kt
+++ app/src/main/java/com/example/testdemo/ideapage.kt
@@ -6,13 +6,12 @@ import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class ideapage: AppCompatActivity(){
-    val textbtn = findViewById<Button>(R.id.txtbtn)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.ideapage)
 
-
+        val textbtn = findViewById<Button>(R.id.txtbtn)
         textbtn.setOnClickListener{
             val i=Intent(this, ChatActivity::class.java)
             startActivity(i);
