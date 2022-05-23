58582f4a1f5acbfbcef07317f6060ecd0d66a762
Sajid Saleem <sajidsaleem864@gmail.com>
2022-05-23 23:56:49 +0500

    work done

--- app/src/main/java/com/example/testdemo/useridea.kt
+++ app/src/main/java/com/example/testdemo/useridea.kt
@@ -1,4 +1,11 @@
 package com.example.testdemo
 
-class useridea {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class useridea: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.userpage)
+    }
 }
